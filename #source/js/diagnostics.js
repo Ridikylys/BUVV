9bcb8098cc12486d45353b80f2894ceacbf48a52
Ridikylys <pawn1738@gmail.com>
2025-06-27 15:17:34 +0300

    fix bugs

--- #source/js/diagnostics.js
+++ #source/js/diagnostics.js
@@ -7,7 +7,7 @@ $('.diagnostics-legend').click(function(event) {
 	$('.diagnostics-legend-arrows').toggleClass('diagnostics-legend-arrows-up');
 	$('.diagnostics-legend-arrowsDown').toggleClass('diagnostics-legend-arrowsDown-visible');
 	$('.diagnostics-block').toggleClass('displaynone2');
-	$('.diagnostics-filter').toggleClass('displaynone');
+	$('.diagnostics-filter').toggleClass('displaynone4');
 });
 
 
